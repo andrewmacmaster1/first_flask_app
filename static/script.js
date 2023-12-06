8a35471262c202746d525cd420dae7b20735fe07
MacMaster <andrew.macmaster@fiserv.com>
2023-12-06 15:22:15 -0500

    Fully implemented db, currently fighting with dark mode

--- static/script.js
+++ static/script.js
@@ -1,75 +1,75 @@
 var light = true;
 
-function addNewTask() {
-    // fetch input and validate that it isn't empty
-    let input = document.getElementById("new-task").value;
-    if (input.trim() == "") {
-        document.getElementById("new-task").value = "";
-        return;
-    }
-    //fetch incomplete tasks and instantiate new list element
-    let list = document.getElementById("incomplete-tasks");
-    var li = document.createElement("li");
+// function addNewTask() {
+//     // fetch input and validate that it isn't empty
+//     let input = document.getElementById("new-task").value;
+//     if (input.trim() == "") {
+//         document.getElementById("new-task").value = "";
+//         return;
+//     }
+//     //fetch incomplete tasks and instantiate new list element
+//     let list = document.getElementById("incomplete-tasks");
+//     var li = document.createElement("li");
 
-    // create checkbox element and add to li
-    var checkbox = document.createElement('input');
-    checkbox.setAttribute("type", "checkbox");
-    // checkbox.setAttribute("checked", "false");
-    // checkbox.setAttribute()
-    checkbox.setAttribute("onclick", "moveTask()");
-    li.appendChild(checkbox);
+//     // create checkbox element and add to li
+//     var checkbox = document.createElement('input');
+//     checkbox.setAttribute("type", "checkbox");
+//     // checkbox.setAttribute("checked", "false");
+//     // checkbox.setAttribute()
+//     checkbox.setAttribute("onclick", "moveTask()");
+//     li.appendChild(checkbox);
 
-    // create label element and add to li
-    var label = document.createElement("label");
-    label.innerText = input;
-    li.appendChild(label);
+//     // create label element and add to li
+//     var label = document.createElement("label");
+//     label.innerText = input;
+//     li.appendChild(label);
 
-    //create edit button and add to li
-    var editButton = document.createElement("button");
-    editButton.setAttribute("class", "edit");
-    if (light) {
-        editButton.classList.toggle("light-mode")
-    } else {
-        editButton.classList.toggle("dark-mode")
-    }
-    editButton.setAttribute("onclick", "editTask()");
-    editButton.innerText = "Edit";
-    li.appendChild(editButton)
+//     //create edit button and add to li
+//     var editButton = document.createElement("button");
+//     editButton.setAttribute("class", "edit");
+//     if (light) {
+//         editButton.classList.toggle("light-mode")
+//     } else {
+//         editButton.classList.toggle("dark-mode")
+//     }
+//     editButton.setAttribute("onclick", "editTask()");
+//     editButton.innerText = "Edit";
+//     li.appendChild(editButton)
     
-    //create edit button and add to li
-    var deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "delete");
-    if (light) {
-        deleteButton.classList.toggle("light-mode")
-    } else {
-        deleteButton.classList.toggle("dark-mode")
-    }
-    deleteButton.setAttribute("onclick", "deleteTask()");
-    deleteButton.innerText = "Delete";
-    li.appendChild(deleteButton)
+//     //create edit button and add to li
+//     var deleteButton = document.createElement("button");
+//     deleteButton.setAttribute("class", "delete");
+//     if (light) {
+//         deleteButton.classList.toggle("light-mode")
+//     } else {
+//         deleteButton.classList.toggle("dark-mode")
+//     }
+//     deleteButton.setAttribute("onclick", "deleteTask()");
+//     deleteButton.innerText = "Delete";
+//     li.appendChild(deleteButton)
     
-    list.appendChild(li);
+//     list.appendChild(li);
 
-    // reset input box
-    document.getElementById("new-task").value = "";
-}
+//     // reset input box
+//     document.getElementById("new-task").value = "";
+// }
 
-function deleteTask() {
-    var currentTask = document.activeElement.parentElement;
-    currentTask.remove();
-}
+// function deleteTask() {
+//     var currentTask = document.activeElement.parentElement;
+//     currentTask.remove();
+// }
 
-function moveTask() {
-    var currentTask = document.activeElement.parentElement;
-    var otherList;
-    if (currentTask.parentElement.id == "incomplete-tasks") {
-        otherList = document.getElementById("completed-tasks");
-    }
-    else if (currentTask.parentElement.id == "completed-tasks") {
-        otherList = document.getElementById("incomplete-tasks");
-    }
-    otherList.appendChild(currentTask);
-}
+// function moveTask() {
+//     var currentTask = document.activeElement.parentElement;
+//     var otherList;
+//     if (currentTask.parentElement.id == "incomplete-tasks") {
+//         otherList = document.getElementById("completed-tasks");
+//     }
+//     else if (currentTask.parentElement.id == "completed-tasks") {
+//         otherList = document.getElementById("incomplete-tasks");
+//     }
+//     otherList.appendChild(currentTask);
+// }
 
 function editTask() {
     var currentTask = document.activeElement.parentElement;
@@ -78,13 +78,14 @@ function editTask() {
 
     var postFrame = document.createElement("form")
     postFrame.setAttribute("method", "post")
+    postFrame.action = `${taskLabel.id}\\edit`
 
     var inputBox = document.createElement("input");
     inputBox.setAttribute("type", "text");
     inputBox.setAttribute("id", taskLabel.id)
     inputBox.setAttribute("value", taskLabel.innerText);
     inputBox.setAttribute("name", "edit_task");
-    inputBox.style.display = 'block';
+    inputBox.style.display = "block";
 
     var saveButton = document.createElement("button");
     saveButton.setAttribute("class", "save");
@@ -96,41 +97,44 @@ function editTask() {
     saveButton.setAttribute("type", "submit");
     saveButton.innerText = "Save";
 
-    currentTask.replaceChild(inputBox, taskLabel);
-    currentTask.replaceChild(saveButton, editButton);
+    postFrame.appendChild(inputBox)
+    postFrame.appendChild(saveButton)
+
+    currentTask.removeChild(taskLabel);
+    currentTask.replaceChild(postFrame, editButton);
 
     
-    parent = currentTask.parentElement;
-    console.log(parent, currentTask);
-    parent.replaceChild(currentTask, postFrame);
-    postFrame.appendChild(currentTask);
+    // parent = currentTask.parentElement;
+    // console.log(parent, currentTask);
+    // parent.replaceChild(currentTask, postFrame);
+    // postFrame.appendChild(currentTask);
 }
 
-function saveTask() {
-    var currentTask = document.activeElement.parentElement;
-    var taskInput = currentTask.children[1];
-    var saveButton = currentTask.children[2];
+// function saveTask() {
+//     var currentTask = document.activeElement.parentElement;
+//     var taskInput = currentTask.children[1];
+//     var saveButton = currentTask.children[2];
 
-    var label = document.createElement("label");
-    label.innerText = taskInput.value;
+//     var label = document.createElement("label");
+//     label.innerText = taskInput.value;
 
-    var editButton = document.createElement("button");
-    editButton.setAttribute("class", "edit");
-    if (light) {
-        editButton.classList.toggle("light-mode")
-    } else {
-        editButton.classList.toggle("dark-mode")
-    }
-    editButton.setAttribute("onclick", "editTask()");
-    editButton.innerText = "Edit";
+//     var editButton = document.createElement("button");
+//     editButton.setAttribute("class", "edit");
+//     if (light) {
+//         editButton.classList.toggle("light-mode")
+//     } else {
+//         editButton.classList.toggle("dark-mode")
+//     }
+//     editButton.setAttribute("onclick", "editTask()");
+//     editButton.innerText = "Edit";
 
-    currentTask.replaceChild(label, taskInput);
-    currentTask.replaceChild(editButton, saveButton);
-}
+//     currentTask.replaceChild(label, taskInput);
+//     currentTask.replaceChild(editButton, saveButton);
+// }
 
-function clearList() {
-    document.getElementById("completed-tasks").innerHTML = "";
-}
+// function clearList() {
+//     document.getElementById("completed-tasks").innerHTML = "";
+// }
 
 function switchTheme() {
     console.clear();
